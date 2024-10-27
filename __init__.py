6d96ecf842a9c30949863c3241347c178685152f
Wirones <138772054+Wirones@users.noreply.github.com>
2024-10-27 13:00:01 +0500

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -13,20 +13,17 @@ tkinter.title("Суммаризатор")
 tkinter.geometry("400x300")
 tkinter.resizable()
 
-def elements_tkinter():
-    tkinter_button_open_file = Button(text="Открыть файл",command=open_file)
-    tkinter_button_open_file.pack()
-    tkinter_button_open_file = Button(text="Сохранить файл",command=save_file)
-    tkinter_button_open_file.pack()
-
 def open_file():
     filepath_open_file = filedialog.askopenfilename()
     with io.open(filepath_open_file, 'r',encoding='utf-8') as f:
         global starting_initial_data
         starting_initial_data = f.read() #Входные данные
-    Label_open_file = Label(text="Ожидание обработки файла")
+    global Label_open_file
+    Label_open_file = Label(text="Идет обработка")
     Label_open_file.pack()
+    destroy_Label(Label_open_file)
     work_summarizer()
+    global Label_save_file 
     Label_save_file = Label(text="Файл готов")
     Label_save_file.pack()
 
@@ -38,6 +35,20 @@ def save_file():
     with open(filepath_save_file, 'w') as file: 
         file.write(full) #Выходные данные в файле
     
+def elements_tkinter():
+    tkinter_button_open_file = Button(text="Открыть файл",command=open_file)
+    tkinter_button_open_file.pack()
+    tkinter_button_save_file = Button(text="Сохранить файл",command=save_file_and_destroy_Label)
+    tkinter_button_save_file.pack()
+
+
+def destroy_Label(Label):
+    Label.destroy()
+
+def save_file_and_destroy_Label():
+    destroy_Label(Label_save_file)
+    save_file()
+
 
 def work_summarizer(): 
     model = Summarizer()  #Импорт бибилиотеки суммиризатора
